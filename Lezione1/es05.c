3249b62f72077c0830190241a001ff2f858d6b69
Riccardo Vescio <101594872+Richard-VR0@users.noreply.github.com>
2025-10-15 19:39:53 +0200

    Delete Lezione1 directory

--- Lezione1/es05.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-    printf("Lun\t10:40\tArchitetture\nMar\t12:10\tBasi di dati\nMer\t13:30\tProgrammazione");
-
-    return 0;
-}
